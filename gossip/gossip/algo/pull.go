cbd3767e6fe1d6aec419b343faf675399e2aad79
unknown <1451670604@qq.com>
2020-11-29 09:07:23 +0800

    update: PullEngine

--- gossip/gossip/algo/pull.go
+++ gossip/gossip/algo/pull.go
@@ -34,8 +34,8 @@ import (
 
 /* PullEngine 是一个基于gossip的pull执行对象，维护一个由字符串标识的元素的内部状态。
 其协议如下：
-1）发起者发送一个带有特定 NONCE 值的 Hello 消息给一组远端节点。
-2）每个远端节点都其消息摘要响应，并返回该 NONCE。
+1）发起者发送一个带有特定 NONCE 值的 Hello 消息给一组远程节点。
+2）每个远程节点都其消息摘要响应，并返回该 NONCE。
 3）发起者检查所有收到的 NONCE 的有效性，聚合所有摘要，并构造一个包含它希望从远
 	程节点获取的所有元素ID的请求包（request），然后将该请求包发送给对应节点。
 4）每个节点都返回一个包含请求元素的响应（response）。前提是，这些节点保留有所
@@ -50,7 +50,7 @@ const (
 
 // DigestFilter filters digests to be sent to a remote peer that
 // sent a hello or a request, based on its messages's context
-// 筛选发送给远端节点的摘要。可能是hello或request摘要，具体根据消息的上下文判断。
+// 筛选发送给远程节点的摘要。可能是hello或request摘要，具体根据消息的上下文判断。
 type DigestFilter func(context interface{}) func(digestItem string) bool
 
 // PullAdapter is needed by the PullEngine in order to
@@ -58,8 +58,8 @@ type DigestFilter func(context interface{}) func(digestItem string) bool
 // The PullEngine expects to be invoked with
 // OnHello, OnDigest, OnReq, OnRes when the respective message arrives
 // from a remote PullEngine
-// PullEngine 需要通过 PullAdapter 发送消息给远端 PullEngine 实例。
-// 当相应的消息从远端 PullEngine 到来时，PullEngine 将会调用OnHello,
+// PullEngine 使用 PullAdapter 发送消息给远程 PullEngine 实例。
+// 当相应的消息从远程 PullEngine 到来时，PullEngine 将会调用OnHello,
 // OnDigest, OnReq, OnRes
 type PullAdapter interface {
 	// SelectPeers returns a slice of peers which the engine will initiate the protocol with
@@ -75,36 +75,36 @@ type PullAdapter interface {
 
 	// SendDigest sends a digest to a remote PullEngine.
 	// The context parameter specifies the remote engine to send to.
-	// 发送一个摘要给远端PullEngine。
-	// context 参数指定要发送到的远端引擎。
+	// 发送一个摘要给远程PullEngine。
+	// context 参数指定要发送到的远程引擎。
 	SendDigest(digest []string, nonce uint64, context interface{})
 
 	// SendReq sends an array of items to a certain remote PullEngine identified
 	// by a string
-	// 发送 items 到一个由 dest 指定地址的远端 PullEngine
+	// 发送 items 到一个由 dest 指定地址的远程 PullEngine
 	SendReq(dest string, items []string, nonce uint64)
 
 	// SendRes sends an array of items to a remote PullEngine identified by a context.
-	// 发送 items 到一个由 context 指定地址的远端 PullEngine
+	// 发送 items 到一个由 context 指定地址的远程 PullEngine
 	SendRes(items []string, context interface{}, nonce uint64)
 }
 
 // PullEngine is the component that actually invokes the pull algorithm
 // with the help of the PullAdapter
-// PullEngine 是通过PullAdapter的帮助实际调用pull算法的组件
+// PullEngine 是在 PullAdapter 的帮助下实际调用 pull 算法的组件
 type PullEngine struct {
-	PullAdapter
-	stopFlag           int32
-	state              *util.Set
-	item2owners        map[string][]string
-	peers2nonces       map[string]uint64 // <endpoint, nonce> endpoint: 端点地址
-	nonces2peers       map[uint64]string // <nonce, endpoint>
-	acceptingDigests   int32
-	acceptingResponses int32
+	PullAdapter                    // pull 适配器
+	stopFlag           int32               // 停止标志
+	state              *util.Set           // 保存消息摘要的集合，其线程安全
+	item2owners        map[string][]string // item所对应拥有该item的拥有者集合
+	peers2nonces       map[string]uint64   // <endpoint, nonce> endpoint: 端点地址
+	nonces2peers       map[uint64]string   // <nonce, endpoint>
+	acceptingDigests   int32               // 正在接收摘要
+	acceptingResponses int32               // 正在接收回复
 	lock               sync.Mutex
 	outgoingNONCES     *util.Set
 	incomingNONCES     *util.Set
-	digFilter          DigestFilter
+	digFilter          DigestFilter	// 摘要过滤器
 
 	digestWaitTime   time.Duration
 	requestWaitTime  time.Duration
@@ -112,10 +112,11 @@ type PullEngine struct {
 }
 
 // PullEngineConfig is the configuration required to initialize a new pull engine
+// PullEngineConfig 是初始化一个pull引擎的所需要的配置
 type PullEngineConfig struct {
-	DigestWaitTime   time.Duration
-	RequestWaitTime  time.Duration
-	ResponseWaitTime time.Duration
+	DigestWaitTime   time.Duration // 摘要等待时间
+	RequestWaitTime  time.Duration // 请求等待时间
+	ResponseWaitTime time.Duration // 响应等待时间
 }
 
 // NewPullEngineWithFilter creates an instance of a PullEngine with a certain sleep time
@@ -196,29 +197,33 @@ func (engine *PullEngine) initiatePull() {
 	engine.lock.Lock()
 	defer engine.lock.Unlock()
 
+	// 1、发送Hello消息
 	engine.acceptDigests()
 	for _, peer := range engine.SelectPeers() {
-		nonce := engine.newNONCE()
-		engine.outgoingNONCES.Add(nonce)
-		engine.nonces2peers[nonce] = peer
-		engine.peers2nonces[peer] = nonce
-		engine.Hello(peer, nonce)
+		nonce := engine.newNONCE()        // 获取一个唯一的nonce值
+		engine.outgoingNONCES.Add(nonce)  // 保存发送的nonce
+		engine.nonces2peers[nonce] = peer // 记录nonce所对应的peer（注：这里的peer指的是节点对应的地址）
+		engine.peers2nonces[peer] = nonce // 记录peer所对应的nonce
+		engine.Hello(peer, nonce)         // 向peer发送Hello消息
 	}
 
+	// 2、在 digestWaitTime 时间后开始调用processIncomingDigests，对到来Digest进行处理
 	time.AfterFunc(engine.digestWaitTime, func() {
 		engine.processIncomingDigests()
 	})
 }
 
+// processIncomingDigests 处理到来的摘要
 func (engine *PullEngine) processIncomingDigests() {
-	engine.ignoreDigests()
+	engine.ignoreDigests() // 忽略接下来收到的 Digest
 
-	engine.lock.Lock()
+	engine.lock.Lock() // lock
 	defer engine.lock.Unlock()
 
+	// 1、组装待发送的 request 的内容
 	requestMapping := make(map[string][]string)
 	for n, sources := range engine.item2owners {
-		// select a random source
+		// 随机选择一个源(摘要拥有者)
 		source := sources[util.RandomInt(len(sources))]
 		if _, exists := requestMapping[source]; !exists {
 			requestMapping[source] = make([]string, 0)
@@ -227,12 +232,15 @@ func (engine *PullEngine) processIncomingDigests() {
 		requestMapping[source] = append(requestMapping[source], n)
 	}
 
+	// 2、允许OnRest处理 Response 消息
 	engine.acceptResponses()
 
+	// 3、发送 Request 消息
 	for dest, seqsToReq := range requestMapping {
 		engine.SendReq(dest, seqsToReq, engine.peers2nonces[dest])
 	}
 
+	// 4、在responseWaitTime时间后调用endPull
 	time.AfterFunc(engine.responseWaitTime, engine.endPull)
 }
 
@@ -249,7 +257,9 @@ func (engine *PullEngine) endPull() {
 }
 
 // OnDigest notifies the engine that a digest has arrived
+// OnDigest 通知引擎一个摘要已经到达
 func (engine *PullEngine) OnDigest(digest []string, nonce uint64, context interface{}) {
+	// 1、若正在接收摘要或不存在该nonce，则直接返回
 	if !engine.isAcceptingDigests() || !engine.outgoingNONCES.Exists(nonce) {
 		return
 	}
@@ -257,15 +267,17 @@ func (engine *PullEngine) OnDigest(digest []string, nonce uint64, context interf
 	engine.lock.Lock()
 	defer engine.lock.Unlock()
 
+	// 2、由获取到的摘要信息，筛选出自身缺失的摘要后将nonce保存，便于后续请求缺失的摘要。
 	for _, n := range digest {
-		if engine.state.Exists(n) {
+		if engine.state.Exists(n) { // 是否已存在该摘要
 			continue
 		}
 
 		if _, exists := engine.item2owners[n]; !exists {
-			engine.item2owners[n] = make([]string, 0)
+			engine.item2owners[n] = make([]string, 0) // 初始化
 		}
 
+		// 将拥有该摘要的节点所对应的nonce值保存
 		engine.item2owners[n] = append(engine.item2owners[n], engine.nonces2peers[nonce])
 	}
 }
@@ -285,14 +297,20 @@ func (engine *PullEngine) Remove(seqs ...string) {
 }
 
 // OnHello notifies the engine a hello has arrived
+// OnHello 通知引擎一个 hello 已经到达
 func (engine *PullEngine) OnHello(nonce uint64, context interface{}) {
+	// 1、将 nonce 记录到 incomingNONCES
 	engine.incomingNONCES.Add(nonce)
 
+	// 2、若是在 requestWaitTime 时间内没有收到 request，则移除 nonce
 	time.AfterFunc(engine.requestWaitTime, func() {
 		engine.incomingNONCES.Remove(nonce)
 	})
 
+	// 3、获取所有保存在集合中的消息摘要
 	a := engine.state.ToArray()
+
+	// 4、由filter筛选出所有符合要求的摘要(digest)
 	var digest []string
 	filter := engine.digFilter(context)
 	for _, item := range a {
@@ -302,6 +320,8 @@ func (engine *PullEngine) OnHello(nonce uint64, context interface{}) {
 		}
 		digest = append(digest, dig)
 	}
+
+	// 5、交由PullAdapter发送摘要信息
 	if len(digest) == 0 {
 		return
 	}
@@ -309,13 +329,15 @@ func (engine *PullEngine) OnHello(nonce uint64, context interface{}) {
 }
 
 // OnReq notifies the engine a request has arrived
+// OnReq 处理到达的 request 消息
 func (engine *PullEngine) OnReq(items []string, nonce uint64, context interface{}) {
-	if !engine.incomingNONCES.Exists(nonce) {
+	if !engine.incomingNONCES.Exists(nonce) { // 是否保存交互的nonce值
 		return
 	}
 	engine.lock.Lock()
 	defer engine.lock.Unlock()
 
+	// 1、筛选远程节点请求的元素(即消息摘要)，并将元素加入待发送序列(items2Send)中
 	filter := engine.digFilter(context)
 	var items2Send []string
 	for _, item := range items {
@@ -328,10 +350,12 @@ func (engine *PullEngine) OnReq(items []string, nonce uint64, context interface{
 		return
 	}
 
+	// 2、异步发送响应消息
 	go engine.SendRes(items2Send, context, nonce)
 }
 
 // OnRes notifies the engine a response has arrived
+// OnRes 通知引擎一个响应已经到达
 func (engine *PullEngine) OnRes(items []string, nonce uint64) {
 	if !engine.outgoingNONCES.Exists(nonce) || !engine.isAcceptingResponses() {
 		return
@@ -340,6 +364,7 @@ func (engine *PullEngine) OnRes(items []string, nonce uint64) {
 	engine.Add(items...)
 }
 
+// 返回一个随机不重复的uint64类型的NONCE值
 func (engine *PullEngine) newNONCE() uint64 {
 	n := uint64(0)
 	for {
